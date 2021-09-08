ee8872b9703034ed02039433471b4b490f8bf084
Camille <cgon@itu.dk>
2021-09-08 18:37:18 +0200

    added a static

--- AS00/Program.cs
+++ AS00/Program.cs
@@ -12,7 +12,7 @@ namespace AS00
         }
 
  
-        public bool IsLeapYear(int year){
+        public static bool IsLeapYear(int year){
             if(year%4 == 0){
                 if(year%100 != 0 || year%400 == 0)
                 return true;
