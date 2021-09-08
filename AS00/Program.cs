c346c643f6057673689e1a8b0b4975b2df3e7cd3
Camille <cgon@itu.dk>
2021-09-08 12:36:34 +0200

    Implemented error handling

--- AS00/Program.cs
+++ AS00/Program.cs
@@ -10,15 +10,36 @@ namespace AS00
             Console.WriteLine("Hello World!");
         }
 
-        public bool IsLeapYear(int year){
+        public static bool IsLeapYear(int year){
             if(year%4 == 0){
-                if(year%100 != 0 || year%400 == 0)
-                return true;
+                if(year%100 != 0 || year%400 == 0){
+                    return true;
+                }
             }else {
                 return false;
             }
 
             return false;
+        }
+
+        public static void UserInputIsLeapYear(){
+            try{
+
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                if(input < 1582){
+                    throw new ArgumentException("The year has to be 1582 or bigger");
+                }
+
+                if(IsLeapYear(input)){
+                    Console.WriteLine("yay");
+                }else{
+                    Console.WriteLine("nay");
+                }
+
+            }catch(ArgumentException e){
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
